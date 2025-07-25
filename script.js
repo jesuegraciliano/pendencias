171156dc67a7a2397b685808475cc6525fe96b2c
JESUEGRACILIANO <113379356+jesuegraciliano@users.noreply.github.com>
2025-07-25 09:40:16 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Chatbot IFSC</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="cabecalho">
+        <img src="logo-ifsc.png" alt="Logotipo IFSC" class="logo-ifsc">
+        <h1>Chatbot MINHAS PENDÊNCIAS </h1>
+    </header>
+
+    <main class="chat-container">
+        <div id="chat-messages" class="chat-box"></div>
+        <input type="text" id="user-input" placeholder="Digite sua pergunta aqui..." />
+        <button id="send-button">Enviar</button>
+    </main>
+
+    <script src="script.js"></script>
+</body>
+</html>
